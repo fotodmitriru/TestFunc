f32342b3821e6f9b0e584c985291071033d2f5fe
dmitriRu <djuceret@gmail.com>
2021-12-26 18:27:32 +0200

    First commit. Пример использования Func и Expression

---
+++ TestFunc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace TestFunc
+{
+    class Program
+    {
+        static void Main()
+        {
+            Func<int, int> xfunc = LocalFunction;
+            RunFunc(xfunc);
+
+            int num = 11;
+
+            Expression conditionExpr = Expression.Condition(
+                Expression.Constant(num > 10),
+                Expression.Constant("num is greater than 10"),
+                Expression.Constant("num is smaller than 10")
+            );
+
+            Console.WriteLine(conditionExpr.ToString());
+
+            Console.WriteLine(Expression.Lambda<Func<string>>(conditionExpr).Compile()());
+
+            Console.ReadKey();
+        }
+
+        public static void RunFunc(Func<int, int> localFunc)
+        {
+            Console.WriteLine(localFunc.Invoke(3));
+        }
+
+        public static int LocalFunction(int z)
+        {
+            return ++z;
+        }
+    }
+}
